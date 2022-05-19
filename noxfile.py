d66a4289c1b72650d2b171be624e5a5e34d5b31b
Circuit <circuitsacul@icloud.com>
2022-05-18 21:59:11 -0400

    Initial File Structure

---
+++ noxfile.py
@@ -0,0 +1,36 @@
+import nox
+
+
+@nox.session
+def pytest_and_mypy(session: nox.Session) -> None:
+    session.install("poetry")
+    session.run("poetry", "install")
+
+    session.run("mypy", ".")
+    session.run(
+        "poetry",
+        "run",
+        "python",
+        "-m",
+        "pytest",
+        "--cov=sgrammarbot/",
+        "--cov-report=xml",
+    )
+
+
+@nox.session
+def flake8(session: nox.Session) -> None:
+    session.install("flake8")
+    session.run("flake8")
+
+
+@nox.session
+def black(session: nox.Session) -> None:
+    session.install("black")
+    session.run("black", ".", "--check")
+
+
+@nox.session
+def isort(session: nox.Session) -> None:
+    session.install("isort")
+    session.run("isort", ".", "--check")
