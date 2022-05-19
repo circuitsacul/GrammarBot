42806bf1741d1c5b008b6f487eb6ca45521abcc2
Circuit <circuitsacul@icloud.com>
2022-05-18 22:31:20 -0400

    Basic bot

---
+++ grammarbot/bot.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import hikari
+import crescent
+
+from .config import CONFIG
+
+
+class GrammarBot(crescent.Bot):
+    def __init__(self) -> None:
+        super().__init__(CONFIG.discord_token)
+
+        self.plugins.add_plugin(plugin)
+
+
+plugin = crescent.Plugin("main-plugin")
+
+
+@plugin.include
+@crescent.event
+async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
+    if event.author.is_bot:
+        return
+
+    await event.message.respond("Hello, world!")
