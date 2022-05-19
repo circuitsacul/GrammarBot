6e67ae2190c26a1cc6c5d9ddc8c81241d7578ae4
CircuitSacul <circuitsacul@icloud.com>
2022-05-19 00:19:43 -0400

    Grammar check (#3)
    * Basic working grammar bot
    
    * Improve error messages, fix mypy

--- grammarbot/bot.py
+++ grammarbot/bot.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from typing import cast
+
 import crescent
 import hikari
+import language_tool_python
 
 from .config import CONFIG
 
@@ -11,6 +14,7 @@ class GrammarBot(crescent.Bot):
         super().__init__(CONFIG.discord_token)
 
         self.plugins.add_plugin(plugin)
+        self.tool = language_tool_python.LanguageTool("en-US")
 
 
 plugin = crescent.Plugin("main-plugin")
@@ -19,7 +23,27 @@ plugin = crescent.Plugin("main-plugin")
 @plugin.include
 @crescent.event
 async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
+    bot = cast(GrammarBot, event.app)
+
     if event.author.is_bot:
         return
 
-    await event.message.respond("Hello, world!")
+    if event.content is None:
+        return
+
+    errors: list[language_tool_python.Match] = bot.tool.check(event.content)
+    if not errors:
+        return
+
+    embed = hikari.Embed(description=f"Found {len(errors)} possible error(s).")
+    for error in errors[0:25]:
+        embed.add_field(
+            name=error.ruleId,
+            value=error.message
+            + '\n"'
+            + error.context
+            + '"\n->'
+            + ", ".join(error.replacements[0:5]),
+        )
+
+    await event.message.respond(embed=embed, reply=True)
