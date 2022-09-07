e5db742fe876cad5decdc05f6cf2bf343df393f1
CircuitSacul <circuitsacul@icloud.com>
2022-09-07 00:11:55 +0000

    fix problems caused by new hikari-crescent version

---
+++ grammarbot/plugin.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING, cast
+
+import crescent
+import hikari
+import language_tool_python
+
+if TYPE_CHECKING:
+    from .bot import GrammarBot
+
+
+plugin = crescent.Plugin()
+
+
+@plugin.include
+@crescent.event
+async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
+    bot = cast(GrammarBot, event.app)
+
+    if event.author.is_bot:
+        return
+
+    if event.content is None:
+        return
+
+    errors: list[language_tool_python.Match] = bot.tool.check(event.content)
+    if not errors:
+        return
+
+    embed = hikari.Embed(description=f"Found {len(errors)} possible error(s).")
+    for error in errors[0:25]:
+        embed.add_field(
+            name=error.ruleId,
+            value=error.message
+            + '\n"'
+            + error.context
+            + '"\n->'
+            + ", ".join(error.replacements[0:5]),
+        )
+
+    await event.message.respond(embed=embed, reply=True)
